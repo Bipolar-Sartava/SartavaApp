f112dbcc189a4acbe9c6e20b63684b3c10bb1f82
orim553 <orim553@gmail.com>
2016-01-13 19:52:16 +0200

    Side Menu works good

---
+++ Sartavahackaton/HomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeViewController.swift
+//  Sartavahackaton
+//
+//  Created by ori mizrachi on 1/13/16.
+//  Copyright © 2016 B-Up!. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var btnMenu: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Menu Btn Stuff
+        if self.revealViewController() != nil {
+            btnMenu.target = self.revealViewController()
+            btnMenu.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        //Side Menu Custoization
+        revealViewController().rearViewRevealWidth = 160
+        revealViewController().rearViewRevealDisplacement = 60
+        revealViewController().frontViewShadowRadius = 25
+        revealViewController().frontViewShadowOffset = CGSizeMake(0.0, 2.5)
+        revealViewController().frontViewShadowOpacity = 1.0
+        revealViewController().frontViewShadowColor = UIColor.blackColor()
+        revealViewController().toggleAnimationDuration = 0.7
+    }
+
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
---
+++ Sartavahackaton/SearchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SearchViewController.swift
+//  Sartavahackaton
+//
+//  Created by ori mizrachi on 1/13/16.
+//  Copyright © 2016 B-Up!. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var btnMenu: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Menu Btn Stuff
+        if self.revealViewController() != nil {
+            btnMenu.target = self.revealViewController()
+            btnMenu.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
