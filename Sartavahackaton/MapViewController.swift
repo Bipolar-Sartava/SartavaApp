14dd3f9752438e3451c5fe6b00496635e40e6d0b
orim553 <orim553@gmail.com>
2016-01-14 21:12:41 +0200

    added the Logo and Added some More Pages

---
+++ Sartavahackaton/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  Sartavahackaton
+//
+//  Created by ori mizrachi on 1/14/16.
+//  Copyright © 2016 B-Up!. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var btnMenu: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Side Menu Stuff
+        if self.revealViewController() != nil {
+            btnMenu.target = self.revealViewController()
+            btnMenu.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
